fcad181c8206c4e0f97da7666b8460a4a14c1028
Viktor Milosavljevic <milosavljevic.viktor@gmail.com>
2021-06-11 22:21:22 +0200

    add heroku env vars and posgres url

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from urllib.parse import urlparse, urljoin
 from functools import wraps
 from flask_gravatar import Gravatar
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
